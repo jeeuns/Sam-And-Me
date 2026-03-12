bc60e1527b3ef5c6051c7a754d4c7f1d1ac500bf
Jieun <85708606+jeeuns@users.noreply.github.com>
2026-03-11 23:19:18 -0700

    Add files via upload

---
+++ assets/testset.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="test" tilewidth="16" tileheight="16" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="floor.png" width="16" height="16"/>
+ </tile>
+ <tile id="1">
+  <image source="purple.png" width="16" height="16"/>
+ </tile>
+ <tile id="2">
+  <image source="void.png" width="16" height="16"/>
+ </tile>
+</tileset>
